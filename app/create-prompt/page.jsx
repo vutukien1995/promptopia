2e70a4a87be7dfbf54d705c85440e4c41aa42061
Vũ Tự Kiên <kienonwork@gmail.com>
2023-11-10 06:38:07 +0000

    Update 3 files

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,33 +1,32 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Form from "@components/Form"
+import Form from "@components/Form";
 
 const page = () => {
-    const [submitting, setSubmitting] = useState(false)
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: ''
-    })
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
-    const createPrompt = async (e) => {
-        e.preventDefault()
-        console.log(e.value)
-        setSubmitting(!submitting)
-    }
+  const createPrompt = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+  };
 
-    return (
-        <Form
-            type="Create"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={createPrompt}
-        />
-    )
-}
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default page
+export default page;
